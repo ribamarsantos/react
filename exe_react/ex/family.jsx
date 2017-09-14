d1ba6326e4fa4dfd8696e5843e14876ab2532eb0
Ribamar Santos <ribamaar@gmail.com>
2017-09-14 14:26:06 -0300

    finalizando exercicio

--- exe_react/ex/family.jsx
+++ exe_react/ex/family.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { childrenWithProps} from '../utils/reactUtils.js'
 // de onde vai clonar e  o 2 o que vai  clonar
 // transforma cada um dos elementos no map
+// usando expressao dentro do jsx chamando uma funcao {  }
 export default props => (
     <div>
         <h1> Família</h1>                
