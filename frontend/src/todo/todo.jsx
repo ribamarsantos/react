277ff28c396e7b565219c697c59ce772f18cfce1
Ribamar Santos <ribamaar@gmail.com>
2017-09-19 08:20:23 -0300

    criando refresh

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -14,8 +14,13 @@ export default class Todo extends Component{
         this.state = { description: '', list: []}
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange=this.handleChange.bind(this)
+        this.refresh()
     }
     
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+             .then(resp=>this.setState({...this.state, description: '', list: resp.data}))
+    }
     handleChange(e){
         this.setState({...this.state, description: e.target.value})
     }
