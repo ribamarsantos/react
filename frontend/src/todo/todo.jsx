1cede9a2086fa67890f4a53d21a017813920c13a
Ribamar Santos <ribamaar@gmail.com>
2017-09-19 09:22:33 -0300

    ações de excluir e atuailzar como done

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -12,9 +12,14 @@ export default class Todo extends Component{
     constructor(props){
         super(props)
         this.state = { description: '', list: []}
+
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange=this.handleChange.bind(this)
+
         this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+
         this.refresh()
     }
     
@@ -36,14 +41,29 @@ export default class Todo extends Component{
         axios.delete(`${URL}/${todo._id}`)
              .then(resp => this.refresh())
     }
+    handleMarkAsDone(todo){
+        axios.put(`${URL}/${todo._id}`, { ...todo, done:true})
+             .then(resp => this.refresh())
+    }
+    handleMarkAsPending(todo){
+        axios.put(`${URL}/${todo._id}`, { ...todo, done:false})
+             .then(resp => this.refresh())
+    }
    render() {
        return (
            <div>
                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
                <TodoForm description={this.state.description}
                          handleChange={this.handleChange}
-                         handleAdd={this.handleAdd} />
-               <TodoList list ={this.state.list} handleRemove={this.handleRemove} />
+                         handleAdd={this.handleAdd}
+                         handleMarkAsDone={this.handleMarkAsDone}
+                         handleMarkAsPending={this.handleMarkAsPending}
+                          />
+               <TodoList list ={this.state.list} 
+                         handleRemove={this.handleRemove} 
+                         handleMarkAsDone={this.handleMarkAsDone}
+                         handleMarkAsPending={this.handleMarkAsPending}
+                         />
            </div>
        )
    }   
