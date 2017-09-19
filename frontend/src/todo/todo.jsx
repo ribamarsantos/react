7046918bd0ad86302001ae1177f4ed0126d29e15
Ribamar Santos <ribamaar@gmail.com>
2017-09-19 08:07:07 -0300

    evento onChange

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -7,9 +7,14 @@ export default class Todo extends Component{
 
     constructor(props){
         super(props)
+        this.state = { description: '', list: []}
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange=this.handleChange.bind(this)
     }
     
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value})
+    }
     handleAdd(){
         console.log('====================================');
         console.log('clicou');
@@ -19,7 +24,9 @@ export default class Todo extends Component{
        return (
            <div>
                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-               <TodoForm handleAdd={this.handleAdd} />
+               <TodoForm description={this.state.description}
+                         handleChange={this.handleChange}
+                         handleAdd={this.handleAdd} />
                <TodoList />
            </div>
        )
