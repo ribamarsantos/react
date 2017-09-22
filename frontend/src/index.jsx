c1ded494e4e24faa38214032d1f949b3a68eaf14
Ribamar Santos <ribamaar@gmail.com>
2017-09-22 11:34:04 -0300

    ajuste no add para realizar search com middleware redux-thunk

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,9 +6,10 @@ import { Provider } from 'react-redux'
 import reducers from './main/reducers'
 import promise from 'redux-promise'
 import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(multi,promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(thunk,multi,promise)(createStore)(reducers, devTools)
 
 import App from './main/app'
 
