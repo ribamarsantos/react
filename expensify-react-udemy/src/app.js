d6b6baf190a5ee495cf5469d7c15df52fcd8c04f
Ribamar Santos <ribamaar@gmail.com>
2018-01-19 11:51:40 -0300

    testing refactor still work

--- expensify-react-udemy/src/app.js
+++ expensify-react-udemy/src/app.js
@@ -4,9 +4,24 @@ import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import  getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
-console.log(store.getState());
+store.subscribe(() => {
+     const state = store.getState();
+     const visibleExpense = getVisibleExpenses(state.expenses, state.filters);
+     console.log(visibleExpense);
+ });
+
+// adding an expense
+store.dispatch(addExpense({description: 'Water Bill', note:'JAN/2018', amount: 4100, createdAt: 125}));
+store.dispatch(addExpense({description: 'Gas Bill', note:'JAN/2018', amount: 5000, createdAt: 125}));
+// filter expense
+ store.dispatch(setTextFilter('Gas'));
+
+//console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
